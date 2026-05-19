8c22dde2f0727b38707a46b22db3359f79572e74
Jasper <jasperjchristian22@gmail.com>
2026-05-19 19:04:22 -0400

    Initial Commit

---
+++ include/ConfigParser.h
@@ -0,0 +1,13 @@
+//
+// Created by Jasper on 2026-05-19.
+//
+
+#ifndef STATICTRACE_CONFIGPARSES_H
+#define STATICTRACE_CONFIGPARSES_H
+
+
+class ConfigParser {
+};
+
+
+#endif //STATICTRACE_CONFIGPARSES_H
